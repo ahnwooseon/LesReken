eafc7b92a5a2ee11826e499b98684a37b72da945
Ahn Wooseon <ahnwooseon@gmail.com>
2026-03-16 09:19:16 +0100

    feat: remove apiservice.

--- src/LesReken.AppHost/AppHost.cs
+++ src/LesReken.AppHost/AppHost.cs
@@ -1,14 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder
-    .AddProject<Projects.LesReken_ApiService>("apiservice")
-    .WithHttpHealthCheck("/health");
-
 builder
     .AddProject<Projects.LesReken_Web>("webfrontend")
     .WithExternalHttpEndpoints()
-    .WithHttpHealthCheck("/health")
-    .WithReference(apiService)
-    .WaitFor(apiService);
+    .WithHttpHealthCheck("/health");
 
 builder.Build().Run();
